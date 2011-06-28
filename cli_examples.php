13815431c9bf81b4cfd10f2c4148c1c865e76ab4
David Harker <github.lists@oneiria.co.uk>
2011-06-28 13:45:00 +0100

    Added path helper functions. cli_examples.php is pretty much finished now hopefully.

--- cli_examples.php
+++ cli_examples.php
@@ -34,22 +34,35 @@ elseif (isset ($opts['i']) && !isset ($examples[$opts['i']]))
     $dieHelp = "Index not found";
 elseif (isset ($opts['e']) && !in_array($opts['e'], $examples))
     $dieHelp = "Example name not found";
-if ($dieHelp !== FALSE) {
-    echo "!" . $dieHelp . "\nUsage:\n\tcli_examples.php -i<array index of example>\n\tcli_examples.php -e<name of example>\nAvailable examples:\n" . print_r ($examples, TRUE);
-    exit (0);
-}
+if ($dieHelp !== FALSE)
+    die ("!" . $dieHelp . "\nUsage:\n\tcli_examples.php -i<array index of example>\n\tcli_examples.php -e<name of example>\nAvailable examples:\n" . print_r ($examples, TRUE));
 elseif (isset ($opts['i']))
     $example = $examples[$opts['i']];
 elseif (isset ($opts['e']))
     $example = $opts['e'];
 
+define ('EXAMPLE_NAME', $example);
+
+// Define a few helper functions for the example
+function examples_data_path () {
+    return dirname(realpath (__FILE__)).'/example_data/';
+}
+function examples_output_path ($name = NULL) {
+    static $n = NULL; $n = ($n === NULL) ? $n : $name; // Only allow name to be set once
+    $path = dirname(realpath (__FILE__)).'/output/' . $n . '/';
+    if (!is_dir ($path))
+        mkdir ($path) or die ("Couldn't create $path to save output :-(\n");
+}
+
+// Load TTKPL
+echo "Loading TTKPL...\n";
+require_once 'lib/ttkpl/lib/ttkpl.php';
 // Load example
 $expath = $path.$example.'.example.php';
-echo "Loading example: $expath...\n";
-include $expath or die ("Failed to load example!");
+echo "\nLoading example: $expath...\n";
+include ($expath);
 
-require_once 'lib/ttkpl/lib/ttkpl.php';
+// Example file can take it from here!
 
-
-echo "\n";
+echo "\nFinito!\n";
 ?>
