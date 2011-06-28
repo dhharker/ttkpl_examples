721d86492c28d7e6ef461d468d5a620122451950
David Harker <github.lists@oneiria.co.uk>
2011-06-28 13:45:31 +0100

    added template example with ref to path helper functions

---
+++ examples/template.example.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * This is the template for new examples.
+ * Examples start with TTKPL already loaded
+ *
+ */
+
+// Output path is created automatically if it doesn't exist, full path returned.
+$output_path = examples_output_path(EXAMPLE_NAME);
+// Returns path to example data. This is shared and returns the full path whether or not it exists.
+$data_path = examples_data_path();
+
+?>
