205de74512b0e41cb1c12dada9d0a928dc5dd22e
David Harker <github.lists@oneiria.co.uk>
2011-06-28 12:58:08 +0100

    added ttkpl as submodule plus examples launcher stub

---
+++ cli_examples.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of cli_examples
+ *
+ * @author david
+ */
+class cli_examples {
+    //put your code here
+}
+?>
