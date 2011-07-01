cb6665b02963a1f9a54a3e343b28904835d168cd
David Harker <github.lists@oneiria.co.uk>
2011-07-01 05:20:59 +0100

    good start on 7 but not currently working, some problem getting temperature from dbs

---
+++ examples/07_thermal_age.example.php
@@ -0,0 +1,89 @@
+<?php
+
+
+/**
+ * Example #7
+ * What is thermal age and where can I get some?
+ */
+
+/**
+ * Thermal age is a way of normalising time for temperature, it is measured in "10°C thermal years".
+ * The age of a specimen in thermal years is the number of years at a constant 10°C it would have
+ * taken to cause the same amount of degradation to take place. If you insist, you can think of
+ * thermal age being "equivalent years deep in a cave in northern Germany".
+ *
+ * Thermal age is a powerful concept because it allows direct comparison between samples of different
+ * ages, from different sites, time periods or parts of the world entirely. This aids in decision
+ * making on funding provision, destructive sampling of valuable samples as well as helping highlight
+ * contamination.
+ *
+ * Let's get cracking. Most of this example has been covered in previous installments, this is where
+ * we start bringing it all together.
+ *
+ * In this example we'll model a burial that's been under ground for a few thousand years, then dug
+ * up and kept in a box somewhere for a load more years (a habit archaeologists have).
+ */
+
+// First the basics
+$temps = new temperatures ();
+$depurination = new kinetics (126940, 17745329175.856213, "DNA depurination (bone)");
+
+// Where's it?
+$graveLocation = new latLon (51.15579196587064, -2.2412109375); // <-- middle of nowhere
+// What's the weather like there?
+$localisingCorrections = $temps->getPalaeoTemperatureCorrections ($graveLocation);
+
+// What's it buried under?
+$topSoil = new thermalLayer(scalarFactory::makeMetres(0.5), scalarFactory::makeThermalDiffusivity (0.1), "Hypothetical dry, peaty soil.");
+$subSoil = new thermalLayer(scalarFactory::makeMetres(1.8), scalarFactory::makeThermalDiffusivity (0.18), "Hypothetical dry, clay soil.");
+$grave = new burial();
+$grave->addThermalLayer($topSoil);
+$grave->addThermalLayer($subSoil);
+
+// Incidentally, you can give all manner of things to palaeoTime as long as the number is a number of years!
+$today = new palaeoTime ("NOW");
+$excavation = new palaeoTime ("-2");
+$wayBackWhen = new palaeoTime ("4000 bc");
+
+// We've defined everything we need to describe the burial, lets put it all together in a temporothermal
+$timeUnderground = new temporothermal ();
+$timeUnderground->setTempSource ($temps);
+$timeUnderground->setLocalisingCorrections ($localisingCorrections);
+// *Heads up, these are new! (pretty self-explanatory though I hope)
+$timeUnderground->setBurial($grave);
+$timeUnderground->setTimeRange($wayBackWhen, $excavation);
+
+/**
+ * Now for all that time spent in a box. We're not going to model the temperature in the garage or
+ * whatever, we're just going to define a sine for the whole couple of decades because that could be
+ * all the information we've got to work with!
+ */
+$climateInMyGarage = new sine ();
+$climateInMyGarage->setGenericSine(scalarFactory::makeCentigradeAbs(9), 15, 30);
+
+$timeInGarage = new temporothermal();
+$timeInGarage->setConstantClimate($climateInMyGarage);
+$timeInGarage->setTimeRange($excavation, $today);
+
+/**
+ * Up to this point there's not much new, however instead of crunching a load of stuff by hand as
+ * we did in example 4, we'll get to know some of the shortcuts. At this point we've got two
+ * temporothermals, so two spans of time in a known temperature. You know those
+ *          ...shortcuts I mentioned?
+ */
+$thermalAge = new thermalAge ();
+$thermalAge->setKinetics ($depurination);
+$thermalAge->addTemporothermal ($timeInGarage);
+$thermalAge->addTemporothermal ($timeUnderground);
+
+
+$thermalYearsScalar = $thermalAge->getThermalAge ();
+$age = $thermalAge->getAge();
+
+printf ("The sample has an age of %d years versus a thermal age of %d years.",
+        $age,
+        $thermalYearsScalar->getValue()
+        );
+
+//print_r ($thermalAge);
+
