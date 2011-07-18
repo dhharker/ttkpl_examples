f9b4c3ba73bad075d259ee3ad93828b90c7b9af1
David Harker <github.lists@oneiria.co.uk>
2011-07-18 13:22:49 +0100

    added some particularly helpful comments and tweaked the graph to look nicer (wider ranges for complete lines)

--- examples/08_lambda_and_fragment_lengths.example.php
+++ examples/08_lambda_and_fragment_lengths.example.php
@@ -2,19 +2,20 @@
 
 
 /**
+ * You can think of the lambda value as "total breakageness" - it is the amount of damage per unit
+ * time, multiplied by the total time.
  * λ        = k                             × t
  * lambda   = effective rate of reaction    × time spent at effective rate
  * Probability of (b)reakage or (s)urvival of a DNA molecule of (l)ength given λ
  * "Ps(l) = Probability of a DNA molecule of (l)ength base pairs not being broken by depurination"
- * Pb(l)    = λ^(l-1)
- * Ps(l)    = 1 - λ^(l-1)
+ * Pb(l)    = λ*(l-1)
+ * Ps(l)    = (1-λ)^(l-1)
  */
 
-function Pb ($length, $lambda) {
-    $pb = ($length - 1) * $lambda;
-    return $pb;
+function Pb ($length, $lambda) { // probability that any of the bonds in a given DNA strand will break
+    return ($length - 1) * $lambda;
 }
-function Ps ($length, $lambda) {
+function Ps ($length, $lambda) { // probability that a DNA strand of a given length will survive
     return pow (1 - $lambda, $length - 1);
 }
 
@@ -32,7 +33,7 @@ do {
     $plot->setData ("λ = $λ (mfl=$mfl)", ++$gi)
          ->addData ($mfl, Ps ($mfl, $λ), 1);
 
-    for ($l = 2; $l <= $mfl * 3; $l += 1    )
+    for ($l = 0; $l <= $mfl * 6; $l += 1    )
         $plot->addData ($l, Ps ($l, $λ), $gi);
     $λ *= 0.5;
 } while ($λ > 1E-4);
